5862fb82aa1ed97ba5eac011f8b641956b0fbf2b
MamaIceBear <snguyen0788@gmail.com>
2020-01-17 16:56:37 -0800

    CONTROL ISSUES#1

---
+++ ElevatorPitch/Assets/Scripts/Player Control Scripts/Controller.cs
@@ -0,0 +1,45 @@
+﻿//Designed By Sam Nguyen
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controller : MonoBehaviour
+{
+    /*
+    public float speed = 20;
+    InputMaster controls;
+   
+    Vector2 move;
+    
+    void Awake()
+    {
+        controls = new InputMaster();
+        controls.IngameControls.Movement.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.IngameControls.Movement.canceled += ctx => move = Vector2.zero;
+        controls.IngameControls.Movement.performed += ctx => onMovement();
+
+        //controls.IngameControls.Buttons.performed += ctx => move = ctx.ReadValue<>();
+    }
+    */
+    void OnMovement(InputValue value)
+    {
+        Vector2 move = value.Get<Vector2>();
+        Debug.Log(move);
+    }
+    /*
+    private void Update()
+    {
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+    
+    void OnEnable()
+    {
+        controls.IngameControls.Enable();
+    }
+    void OnDisable()
+    {
+        controls.IngameControls.Disable();
+    }
+    */
+}
