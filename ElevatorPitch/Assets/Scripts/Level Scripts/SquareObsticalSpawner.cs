29fa7eda2edb8e8eb898b64bdb4333efe7ba619b
Evan Li <evanli1one@gmail.com>
2020-01-22 18:25:08 -0800

    Mutliple obstacle spawning
    can spawn multiple copies of a prefab at placeable spawn locations.

---
+++ ElevatorPitch/Assets/Scripts/Level Scripts/SquareObsticalSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareObsticalSpawner : MonoBehaviour
+{
+    public GameObject SquareObstaclePrefab;
+    public Transform[] spawnLocations;
+    public GameObject whatToSpawnPrefab;
+    public GameObject whatToSpawnClone;
+
+    public void spawnObstacles()
+    {
+        for(int i = 0; i < spawnLocations.Length; i++)
+        {
+            whatToSpawnClone = Instantiate(whatToSpawnPrefab, spawnLocations[i].transform.position, spawnLocations[i].transform.rotation);
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnObstacles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
