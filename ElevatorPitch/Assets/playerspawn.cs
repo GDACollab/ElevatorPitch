0d85429668108b4006502700206df58f75a1bf91
tmbraswe <tmbraswe@ucsc.edu>
2020-01-24 15:10:50 -0800

    made a basic spawner prefab

---
+++ ElevatorPitch/Assets/playerspawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playerspawn : MonoBehaviour
+{
+    public GameObject cubeObj;
+    public bool respawn = false;
+    //using public
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(cubeObj, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (respawn)
+        {
+            Instantiate(cubeObj, transform.position, transform.rotation);
+        }
+    }
+}
