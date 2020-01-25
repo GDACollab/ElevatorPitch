232ce3aa7edb7c112bf32b5bad11e4ae53e737bb
Brian Quintero <bquinte1@ucsc.edu>
2020-01-24 16:17:25 -0800

    Basic Timer Algorithm

---
+++ ElevatorPitch/Assets/Scripts/Game Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int maxTime = 10;
+
+    void Start()
+    {
+        StartCoroutine(waitTime());
+    }
+    //timer algorithm (source: https://stackoverflow.com/questions/30056471/how-make-the-script-wait-sleep-in-a-simple-way-in-unity)
+    IEnumerator waitTime()
+    {
+        int timeSet = maxTime;
+        while (timeSet >= 0)
+        {
+            yield return new WaitForSeconds(1);
+            Debug.Log(timeSet);
+            timeSet--;
+        }
+    }
+}
