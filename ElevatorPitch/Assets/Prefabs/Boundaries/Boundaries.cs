6d83dc4f6d32912ae2bc2a55e72438733030e6c6
Ian Cowan <icowan@ucsc.edu>
2020-03-01 23:47:27 -0800

    Created boundaries script
    Made a script to replace the edgeCollider system. Attaches to player prefab and clamps the x and y transform of the player to adjustable boundary floats

---
+++ ElevatorPitch/Assets/Prefabs/Boundaries/Boundaries.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    public float horizontalWall;
+    public float verticalWall;
+    private Vector2 bounds;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounds = new Vector2(horizontalWall * -1, verticalWall * -1);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 playerPos = transform.position;
+        playerPos.x = Mathf.Clamp(playerPos.x, bounds.x, bounds.x * -1);
+        playerPos.y = Mathf.Clamp(playerPos.y, bounds.y, bounds.y * -1);
+        transform.position = playerPos;
+    }
+}
