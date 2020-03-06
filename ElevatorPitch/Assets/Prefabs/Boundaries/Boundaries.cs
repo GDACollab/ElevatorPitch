5c167fe68614e6772ae30e8855db4e29fddadcb7
Ian Cowan <icowan@ucsc.edu>
2020-03-05 19:33:15 -0800

    Finished Boundaries script and added player prefab for testing

--- ElevatorPitch/Assets/Prefabs/Boundaries/Boundaries.cs
+++ ElevatorPitch/Assets/Prefabs/Boundaries/Boundaries.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class Boundaries : MonoBehaviour
 {
-    public float horizontalWall;
-    public float verticalWall;
-    private Vector2 bounds;
+    [Tooltip("Distance of left barrier from 0 (negative value).")]
+    public float westWall;
+    [Tooltip("Distance of right barrier from 0 (positive value).")]
+    public float eastWall;
+    [Tooltip("Distance of bottom barrier from 0 (negative value).")]
+    public float southWall;
+    [Tooltip("Distance of top barrier from 0 (positive value).")]
+    public float northWall;
+    private Vector2 boundsHorizontal;
+    private Vector2 boundsVertical;
     
     // Start is called before the first frame update
     void Start()
     {
-        bounds = new Vector2(horizontalWall * -1, verticalWall * -1);
+        boundsHorizontal = new Vector2(northWall, southWall);
+        boundsVertical = new Vector2(eastWall, westWall);
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         Vector3 playerPos = transform.position;
-        playerPos.x = Mathf.Clamp(playerPos.x, bounds.x, bounds.x * -1);
-        playerPos.y = Mathf.Clamp(playerPos.y, bounds.y, bounds.y * -1);
+        playerPos.x = Mathf.Clamp(playerPos.x, boundsVertical.y, boundsVertical.x);
+        playerPos.y = Mathf.Clamp(playerPos.y, boundsHorizontal.y, boundsHorizontal.x);
         transform.position = playerPos;
     }
 }
