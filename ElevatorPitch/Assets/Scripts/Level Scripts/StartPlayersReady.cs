c43d7bc4db93b49d7c2343b5656d1af68720f5e6
ctwatterson <38928567+ctwatterson@users.noreply.github.com>
2020-04-24 17:51:35 -0700

    start screeen and updated level design

---
+++ ElevatorPitch/Assets/Scripts/Level Scripts/StartPlayersReady.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPlayersReady : MonoBehaviour
+{
+    private int playerCount = 0;
+    private void OnTriggerEnter2D(Collider2D other) {
+        playerCount++;
+    }
+}
