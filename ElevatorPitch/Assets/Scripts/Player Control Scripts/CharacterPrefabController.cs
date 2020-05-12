1393c4a89705dc5b2ca2917f28f45d6c56a3e3d6
Jacob Compton <jrcompto@ucsc.edu>
2020-05-12 16:23:35 -0700

    Stuff

--- ElevatorPitch/Assets/Scripts/Player Control Scripts/CharacterPrefabController.cs
+++ ElevatorPitch/Assets/Scripts/Player Control Scripts/CharacterPrefabController.cs
@@ -9,6 +9,9 @@ public class CharacterPrefabController : MonoBehaviour
     public GameObject moveTempPlayer;
     private PlayerInput playerInput;
     private int playerIndex;
+    private GameObject gameModeObj;
+    private int gameMode;
+    private GameObject spawnPoint;
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -16,36 +19,73 @@ public class CharacterPrefabController : MonoBehaviour
         DontDestroyOnLoad(transform);
         SceneManager.sceneLoaded += OnSceneLoaded;
         GetComponent<PlayerControl>().enabled = false;
-        OnSceneLoaded(SceneManager.GetActiveScene(), LoadSceneMode.Single);    
+        OnSceneLoaded(SceneManager.GetActiveScene(), LoadSceneMode.Single);
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if(!(scene.name == "quips"))
+        gameModeObj = GameObject.FindGameObjectWithTag("GameMode");
+        gameMode = gameModeObj.GetComponent<WinCondition>().gameModeTemplate;
+        if (!(scene.name == "quips") && !(scene.name == "CoffeeCupGame"))
         {
-            if(scene.name == "Start") {
+            if (scene.name == "Start")
+            {
                 spawnPlayer(new Vector3(0, 0, 0), 1);
-            } else {
+            }
+            else
+            {
                 spawnPlayer(new Vector3(0, 0, 0), 0);
             }
-            
-        } 
+
+        }
+        else if(scene.name == "CoffeeCupGame")
+        {
+            var a = GetComponent<PlayerControl>();
+            a.enabled = true;
+            spawnPoint = GameObject.FindGameObjectWithTag("spawn" + playerIndex);
+            switch (playerIndex)
+            {
+                case 0:
+                    GameObject blazeArm = GameObject.FindGameObjectWithTag("blazeCoffeeArm");
+                    blazeArm.transform.position = spawnPoint.transform.position;
+                    break;
+                case 1:
+                    GameObject gianArm = GameObject.FindGameObjectWithTag("gianCoffeeArm");
+                    gianArm.transform.position = spawnPoint.transform.position;
+                    break;
+                case 2:
+                    GameObject robynArm = GameObject.FindGameObjectWithTag("robynCoffeeArm");
+                    robynArm.transform.position = spawnPoint.transform.position;
+                    break;
+                case 3:
+                    GameObject yeetArm = GameObject.FindGameObjectWithTag("yeetCoffeeArm");
+                    yeetArm.transform.position = spawnPoint.transform.position;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     public void spawnPlayer(Vector3 pos, int x)
     {
         GameObject p = Instantiate(moveTempPlayer, pos, Quaternion.identity);
         var BC = p.GetComponent<BoxCollider2D>();
-        p.GetComponent<playerSetup>().setup(playerIndex);
+        p.GetComponent<playerSetup>().setup(playerIndex, gameMode);
         var a = GetComponent<PlayerControl>();
         a.enabled = true;
         a.pawn = p;
-        if(x == 1) {
+        a.setGameMode(gameMode); //Update gameMode in PlayerControl so it knows what controls to use
+
+        if (x == 1)
+        {
             p.transform.localScale = new Vector3(.7f, .7f, 1);
-            
+
             BC.offset = new Vector2(0, -1);
             BC.size = new Vector2(1.5f, .5f);
-        } else {
+        }
+        else
+        {
             p.transform.localScale = new Vector3(.25f, .25f, 1);
             BC.offset = new Vector2(0, 0);
             BC.size = new Vector2(1.5f, 3.2f);
@@ -54,6 +94,11 @@ public class CharacterPrefabController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public int getGameMode()
+    {
+        return gameMode;
     }
 }
--- ElevatorPitch/Assets/Scripts/Player Control Scripts/PlayerControl.cs
+++ ElevatorPitch/Assets/Scripts/Player Control Scripts/PlayerControl.cs
@@ -12,34 +12,48 @@ public class PlayerControl : MonoBehaviour
     Vector2 move;
     public float movementSpeed;
     private SpriteRenderer sr;
-    private GameObject gameModeObj;
     private GameObject persistantData;
+    //private GameObject prefabController;
     private int gameMode;
     public GameObject pawn;
 
     bool paused = false;
     int index;
 
+    Vector3 grabPosition;
+    GameObject blazeArm;
+    GameObject gianArm;
+    GameObject robynArm;
+    GameObject yeetArm;
+
     private void Start()
     {
         persistantData = GameObject.FindGameObjectWithTag("persData");
-        gameModeObj = GameObject.FindGameObjectWithTag("GameMode");
-        gameMode = gameModeObj.GetComponent<WinCondition>().gameModeTemplate;
         sr = gameObject.GetComponent<SpriteRenderer>();
         index = gameObject.GetComponent<PlayerInput>().playerIndex;
+        gameMode = gameObject.GetComponent<CharacterPrefabController>().getGameMode();
+        Debug.Log("Gotten gameMode: " + gameMode);
+        if (gameMode == 2)
+        {
+            blazeArm = GameObject.FindGameObjectWithTag("blazeCoffeeArm");
+            gianArm = GameObject.FindGameObjectWithTag("gianCoffeeArm");
+            robynArm = GameObject.FindGameObjectWithTag("robynCoffeeArm");
+            yeetArm = GameObject.FindGameObjectWithTag("yeetCoffeeArm");
+        }
+
     }
 
     //Pause function
     void OnPause()
     {
-        if(Time.timeScale > 0 && !paused)
+        if (Time.timeScale > 0 && !paused)
         {
             PauseMenu.playerPause = index + 1; //Tell pause menu which player paused
             Time.timeScale = 0f;
             paused = true;
             PauseMenu.justPaused = true;
         }
-        else if(paused) //Only the player who paused can unpause
+        else if (paused) //Only the player who paused can unpause
         {
             Time.timeScale = 1f;
             paused = false;
@@ -73,7 +87,6 @@ public class PlayerControl : MonoBehaviour
     void OnMovement(InputValue value)
     {
         move = value.Get<Vector2>();
-        //Debug.Log(move);
     }
 
     void OnUpButton()
@@ -83,7 +96,8 @@ public class PlayerControl : MonoBehaviour
 
     void OnDownButton()
     {
-        if(paused)
+        Debug.Log("South Button Pressed");
+        if (paused)
         {
             Debug.Log("South Button Pressed");
             if (PauseMenu.cursorPosition == 0)
@@ -110,7 +124,9 @@ public class PlayerControl : MonoBehaviour
                 Debug.Log("Exit");
                 Application.Quit(); //Maybe replace this with going to the start screen?
             }
-        } else if(gameMode == 2) {
+        }
+        else if (gameMode == 2)
+        {
             persistantData.GetComponent<persistentData>().buttonMash[index]++;
         }
     }
@@ -118,33 +134,74 @@ public class PlayerControl : MonoBehaviour
     void OnLeftButton()
     {
         Debug.Log("Left Button Pressed");
-        
+
     }
 
     void OnRightButton()
     {
-        Debug.Log("Right Button Pressed");
+        Debug.Log("Right Button Pressed. Gamemode is " + gameMode);
+        if (gameMode == 2)
+        {
+            grabPosition = new Vector3(-1, -1, 0);
+            switch (index)
+            {
+                case 0:
+                    //blazeArm.transform.position = Vector2.Lerp(blazeArm.transform.position, grabPosition, Time.time);
+                    blazeArm.transform.position = Vector2.MoveTowards(blazeArm.transform.position, grabPosition, 2);
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+            //pawn.transform.localPosition += new Vector3(2, 2, 2);
+            //pawn.transform.localPosition = Vector2.MoveTowards(pawn.transform.localPosition, new Vector3(-1, -1, 0), 1);
+            //while (pawn.transform.localPosition != grabPosition)
+            //{
+            //    pawn.transform.localPosition = Vector2.MoveTowards(pawn.transform.localPosition, grabPosition, .01f);
+            //}
+
+
+            //pawn.transform.localPosition = Vector2.Lerp(pawn.transform.localPosition, grabPosition, Time.time);
+        }
     }
 
     void FixedUpdate()
     {
-
         if (pawn != null)
         {
             if (move != Vector2.zero)
             {
-                pawn.transform.localPosition = pawn.transform.localPosition +=
-                new Vector3(move.x * movementSpeed * Time.deltaTime, move.y * movementSpeed * Time.deltaTime);
-                Rotation();
+                if (gameMode != 2) //If in gameMode 2, don't allow movement
+                {
+                    pawn.transform.localPosition = pawn.transform.localPosition += new Vector3(move.x * movementSpeed * Time.deltaTime, move.y * movementSpeed * Time.deltaTime);
+                    Rotation();
+                }
+                else
+                {
+                    //pawn.transform.localPosition = Vector2.Lerp(pawn.transform.localPosition, grabPosition, Time.time/20);
+                    grabPosition = new Vector3(-1, -1, 0);
+                    //blazeArm.transform.position += Vector3.MoveTowards(blazeArm.transform.position, grabPosition, 2 * Time.deltaTime);
+                }
+
             }
         }
     }
 
     void Rotation()
     {
-        float angle = Mathf.Atan2(move.y, move.x) * 180/Mathf.PI;
+        float angle = Mathf.Atan2(move.y, move.x) * 180 / Mathf.PI;
         //Debug.Log(angle);
         pawn.transform.rotation = Quaternion.Slerp(pawn.transform.rotation, Quaternion.Euler(0, 0, angle), interpolation * Time.deltaTime);
+    }
+
+    public void setGameMode(int mode)
+    {
+        gameMode = mode;
     }
 }
 
